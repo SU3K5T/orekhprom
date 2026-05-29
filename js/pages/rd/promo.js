1f8f642fded1fd817a6ccd2e0505149cdc330efd
euphoriaWL <surikat0013@icloud.com>
2026-05-29 19:45:11 +0300

    UPDATE | add promo section

--- js/pages/rd/promo.js
+++ js/pages/rd/promo.js
@@ -2,78 +2,53 @@ class PromoScrollTrigger {
   constructor({ trigger }) {
     this.currentIdx = 0;
     this.trigger = trigger;
+    this.slider = this.trigger.querySelector('.swiper');
+    this.initSwiper();
     this.init();
+  }
+
+  initSwiper() {
+    if (!this.slider) return;
+
+    this.swiper = new Swiper(this.slider, {
+      slidesPerView: "2",
+      direction: "vertical",
+      allowTouchMove: false,
+      watchSlideProgress: true,
+      speed: 800,
+    });
   }
 
   init() {
     if (!this.trigger) return;
 
-    let mm = gsap.matchMedia();
+    const mm = gsap.matchMedia();
 
-    const st = ScrollTrigger.create({
-      trigger: this.trigger,
-      start: 'top +=140',
-      end: '+=2600',
-      pin: true,
-      scrub: true,
-      onUpdate: ({ progress }) => {
-        const targetIdx = Math.floor(progress * 4);
-        if (targetIdx !== this.currentIdx && targetIdx < 4) {
-          this.updateItem(targetIdx);
+    mm.add({
+      isDesktop: "(min-width: 992px)",
+      isMobile: "(max-width: 991px)"
+    }, (context) => {
+      const { isDesktop } = context.conditions;
+      const startValue = isDesktop ? 'top +=140' : 'top +=40';
+
+      const st = ScrollTrigger.create({
+        trigger: this.trigger,
+        start: startValue,
+        end: '+=2600',
+        pin: true,
+        scrub: true,
+        onUpdate: ({ progress }) => {
+          const targetIdx = Math.floor(progress * 4);
+          if (targetIdx !== this.currentIdx && targetIdx < 4) {
+            this.currentIdx = targetIdx;
+            this.swiper.slideTo(targetIdx);
+          }
         }
-      }
-    });
-  }
-
-  updateItem(idx) {
-    const currentActiveEl = this.trigger.querySelector(`[data-item="${this.currentIdx}"]`);
-
-    const nextEl = this.trigger.querySelector(`[data-item="${idx}"]`);
-    console.log(nextEl.offsetTop);
-
-    if (idx > this.currentIdx) {
-      currentActiveEl.style.transform = `translateY(-${currentActiveEl.offsetHeight + 20}px)`;
-
-      const onTransitionEnd = (event) => {
-        if (event.target !== currentActiveEl) return;
-        currentActiveEl.classList.remove('active');
-        currentActiveEl.removeEventListener('transitionend', onTransitionEnd);
+      });
+      return () => {
+        st.kill();
       };
-
-      currentActiveEl.addEventListener('transitionend', onTransitionEnd);
-
-      nextEl.style.transform = `translateY(-${currentActiveEl.offsetHeight}px)`;
-      nextEl.classList.add('active');
-
-    }
-
-    // const currentActiveItem = this.trigger.querySelector('.page-rd-promo__interact-item.active');
-    // const currentActiveItemHeight = currentActiveItem.offsetHeight;
-
-    // const nextItem = this.trigger.querySelector(`[data-item="${idx}"]`);
-    // console.log(nextItem)
-    // const nextItemToTopDistance = nextItem.offsetTop;
-    // console.log(nextItemToTopDistance);
-
-    // if (idx > this.currentIdx) {
-    //   currentActiveItem.style.transform = `translateY(-${currentActiveItemHeight + 20}px)`;
-
-    //   const onTransitionEnd = (event) => {
-    //     if (event.target !== currentActiveItem) return;
-    //     currentActiveItem.classList.remove('active');
-    //     currentActiveItem.removeEventListener('transitionend', onTransitionEnd);
-    //   };
-
-    //   currentActiveItem.addEventListener('transitionend', onTransitionEnd);
-
-    //   nextItem.style.transform = `translateY(-${nextItemToTopDistance}px})`;
-    //   nextItem.classList.add('active');
-
-    // } else {
-    //   currentActiveItem.style.transform = `translateY(0)`;
-    //   currentActiveItem.classList.add('active');
-    // }
-    this.currentIdx = idx;
+    });
   }
 
 }
