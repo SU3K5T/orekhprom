155eb3800823a1c0fc58191ebaca078ad0135d60
euphoriaWL <surikat0013@icloud.com>
2026-06-03 10:08:15 +0300

    UPDATE | add sections

---
+++ js/pages/rd/about.js
@@ -0,0 +1,41 @@
+class RDAbout {
+    constructor(target) {
+        this.target = target;
+        this.activeTitleElement = this.target.querySelector('.page-rd-about__accordion-title-item.active');
+        this.activeImageElement = this.target.querySelector('.page-rd-about__accordion-image-item.active')
+        this.init();
+    }
+
+    init() {
+        const titleList = document.querySelectorAll('.page-rd-about__accordion-title-item');
+        titleList.forEach(titleItem => {
+            titleItem.addEventListener('mouseover', (e) => {
+                this.setActiveElement(e.target.closest("[data-title]"));
+            });
+        });
+    }
+
+    setActiveElement(element) {
+        const elementIndex = element.dataset.title;
+
+        const newActiveTitleElement = this.target.querySelector(`[data-title="${elementIndex}"]`);
+        const newActiveImageElement = this.target.querySelector(`[data-image="${elementIndex}"]`);
+
+        newActiveTitleElement.classList.add('active');
+        newActiveImageElement.classList.add('active');
+
+        this.activeImageElement.classList.remove('active');
+        this.activeTitleElement.classList.remove('active');
+        
+        this.activeImageElement = newActiveImageElement;
+        this.activeTitleElement = newActiveTitleElement;
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll('.page-rd-about__accordion-item');
+    elements.forEach(element => {
+        new RDAbout(element);
+    })
+})
---
+++ js/pages/rd/process.js
@@ -0,0 +1,107 @@
+class ProcessPicker {
+    constructor(target) {
+        this.target = target;
+        this.lastEl = target.querySelector('.last');
+        this.init();
+        this.setLast();
+    }
+
+    init() {
+        this.target.querySelectorAll('[data-number]').forEach(element => {
+            element.addEventListener('click', (e) => {
+                this.moveNumber(e.target);
+            })
+        });
+    }
+
+    setLast() {
+        if (window.matchMedia("(max-width: 767px)").matches) {
+            const distToParent = this.lastEl.offsetTop;
+            this.lastEl.style.transform = `translateY(-${distToParent}px)`;
+        } else {
+            const distToParent = this.lastEl.offsetLeft;
+            this.lastEl.style.transform = `translateX(-${distToParent}px)`;
+        }
+    }
+
+    moveNumber(el) {
+        let lastIndex = Number(this.lastEl.dataset.number);
+        const elIndex = Number(el.dataset.number);
+
+        if (lastIndex < elIndex) {
+            this._moveNumberForward(el)
+        } else if (lastIndex > elIndex) {
+            this._moveNumberBackward(el)
+        } else {
+            return;
+        }
+
+    }
+
+    _moveNumberBackward(el) {
+        let lastIndex = Number(this.lastEl.dataset.number);
+        const elIndex = el.dataset.number;
+
+        while (lastIndex > elIndex) {
+            const movableItem = this.target.querySelector(`[data-number="${lastIndex}"]`);
+
+            if (window.matchMedia("max-width: 767px").matches) {
+                movableItem.style.transform = `translateY(0)`;
+            } else {
+                movableItem.style.transform = `translateX(0)`;
+            }
+
+            lastIndex = lastIndex - 1;
+
+            this.lastEl.classList.remove('active');
+            this._updateLastElement(this.target.querySelector(`[data-number="${lastIndex}"]`))
+
+            this._activateByIndex();
+        }
+    }
+
+    _moveNumberForward(el) {
+        let lastIndex = Number(this.lastEl.dataset.number);
+        const elIndex = el.dataset.number;
+
+        while (lastIndex < elIndex) {
+            const movableItem = this.target.querySelector(`[data-number="${Number(lastIndex) + 1}"]`);
+
+            if (window.matchMedia("(max-width: 767px)").matches) {
+                const deltaDist = movableItem.offsetHeight * (lastIndex + 1);
+                const distToParent = movableItem.offsetTop;
+
+                movableItem.style.transform = `translateY(-${distToParent - deltaDist}px)`;
+            } else {
+                const deltaDist = movableItem.offsetWidth * (lastIndex + 1);
+                const distToParent = movableItem.offsetLeft;
+
+                movableItem.style.transform = `translateX(-${distToParent - deltaDist}px)`;
+            }
+
+            lastIndex = lastIndex + 1;
+            this._updateLastElement(movableItem);
+
+            this._activateByIndex();
+        }
+    }
+
+    _updateLastElement(newElement) {
+        this.lastEl.classList.remove('last');
+        this.lastEl = newElement;
+        this.lastEl.classList.add('active');
+        this.lastEl.classList.add('last');
+    }
+
+    _activateByIndex() {
+        const dataItems = this.target.querySelectorAll('[data-item]');
+        dataItems.forEach(item => item.classList.remove('active'));
+
+        this.target.querySelector(`[data-item="${this.lastEl.dataset.number}"]`).classList.add('active');
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const el = document.querySelector('.js-page-rd-process-section');
+    new ProcessPicker(el);
+})
