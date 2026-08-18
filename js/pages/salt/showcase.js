2dedfd80d9876cfc44992c6631f2a89bfb053589
euphoriaWL <surikat0013@icloud.com>
2026-08-18 14:51:40 +0300

    refactor showcase.js

--- js/pages/salt/showcase.js
+++ js/pages/salt/showcase.js
@@ -1,24 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.querySelector('.js-page-salt-showcase-slider');
-  if (!slider) return;
+class ShowcaseSlider {
+  constructor({ target }) {
+    this.target = target;
+    this.slider = target.querySelector('.js-page-salt-showcase-slider');
+    this.init();
+  }
 
-  new Swiper(slider, {
-    slidesPerView: 1,
-    speed: 600,
-    loop: true,
-    effect: 'creative',
-    creativeEffect: {
-      prev: { opacity: 0, translate: ['-10%', 0, 0] },
-      next: { opacity: 0, translate: ['10%', 0, -1] },
-    },
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true,
-    },
-    navigation: {
-      nextEl: '.js-page-salt-showcase-next',
-      prevEl: '.js-page-salt-showcase-prev',
-    },
+  init() {
+    if (!this.slider) return;
+
+    this.swiper = new Swiper(this.slider, {
+      slidesPerView: 1,
+      speed: 600,
+      loop: true,
+      effect: 'creative',
+      creativeEffect: {
+        prev: { opacity: 0, translate: ['-10%', 0, 0] },
+        next: { opacity: 0, translate: ['10%', 0, -1] },
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+      navigation: {
+        nextEl: '.js-page-salt-showcase-next',
+        prevEl: '.js-page-salt-showcase-prev',
+      },
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.page-salt-showcase').forEach((target) => {
+    new ShowcaseSlider({ target });
   });
 });
